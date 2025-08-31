a9e270af17a2fcc67290a7dad671d3d232c521d2
abinvk <vettikkadanabin@gmail.com>
2025-08-31 22:30:26 +0530

    Add files via upload

---
+++ JAVA SCRIPT/SAMPLE-TS .js
@@ -0,0 +1,669 @@
+//let firstName = "abin"
+//let lastNanme = "vk"
+
+//let user = firstName +" "+ lastNanme
+
+//console.log(typeof user);
+
+//let user
+
+//console.log(user);  
+
+//let data = 8
+ 
+//console.log(typeof data)  
+
+//let ab = 5 > 6
+//console.log(typeof ab);
+
+//let msg = "009090909090909090909009900990909090900909090909009090909090900909090900909";
+
+//onsole.log(msg.length)
+
+//let person = {
+    //name: "Abin vk",
+    //age: 23,
+    //fullstrackDevloper: true,
+    //place: "Malappuram",
+//};
+
+//console.log(person.name););
+
+
+//let student = {
+  //name: "Abin",
+  //marks: {
+    //English: 98,
+   // math: 90,
+   // science: 90,
+   // bca: 99,
+  //},
+  //total: function (){
+    //return this.marks.English + this.marks.math + this.marks.science + this.marks.bca;
+ // },
+  //average: function () {
+   // return this.total() / 4;
+  //}
+//};
+
+//console.log("Total Marks:", student.total());
+//console.log("Average",student.average());
+
+
+
+//let lotof =["a","d","d","a","s","d","k",]
+
+//console.log(lotof[5])
+
+//let fruits =["apple","banana"];
+ //console.log(fruits);
+ //fruits.push("orange");
+
+
+ //console.log(fruits);
+
+ //let fruits =["apple","orange","banana"];
+ //console.log(fruits);
+ //fruits.pop("banana","orange");
+ //console.log(fruits);
+
+
+//let fruits =["apple","orange","mango"];
+ //console.log(fruits);
+
+//fruits.shift();
+//console.log(fruits);
+
+//let fruits = ["banana","apple"]
+//console.log(fruits);
+//fruits.unshift("orange");
+//console.log(
+
+
+//function greet(name){
+ // console.log("welcome" + name);
+
+
+//greet("Abin");
+
+
+//function add(a,b){
+  //return a + b;
+//}
+//let result = add(7, 3);
+//console.log(result);
+
+
+
+//function example() {
+  //et x = 10;
+  //console.log(x);
+//}
+
+//function greet(name = "guest") {
+  //console.log("hello" + name);
+//}
+//greet();
+
+//function greet(name) {
+ // console.log("hi" + name);
+//}
+//greet();
+
+//function welcome(callback){
+  //callback("  abinnnnnneee");
+//}
+//welcome(greet);
+
+
+//let age = 5;
+//if (age >= 18) {
+  //console.log("you can vote");
+//}
+//else {
+  //console.log("you can don't vote")
+//}
+
+
+//let time = 14 ;
+//if(time <=12){
+  //console.log("Good Mornnig");
+//}
+//else{
+  //console.log("Good Afternoon");
+//}
+
+//let num1 = 3;
+//let num2 = 4;
+
+//if(num1 > num2){
+//console.log("num1 is greater");
+
+//}
+//else{
+  //console.log("num2 is smaller");
+//}
+
+
+
+//let num1 = 51;
+//let num2 = 200;
+//let num3 = 500;
+
+
+//23
+//if (average >=90){
+ // console.log("Grade: A+");
+//} else if(average >=80){
+ // console.log("Grade A")
+//} else if(average >=70){
+  //console.log("Grade B")
+//}
+
+//let math = 30;
+//let science = 40;
+//let English = 54;
+
+//if (math >=35){
+  //console.log("math : pass");
+//} else{
+  //console.log("math : fail");
+//}
+
+//if (science >=35){
+  //console.log("science : pass");
+//}else{
+  //console.log("scinece : fail");
+//}
+
+//if ("English >=35"){
+  ///console.log("english : pass");
+//} else{
+  //console.log("english ; fail");
+//} 
+
+
+//let option = 0;
+
+//switch(option){
+  //case 1:
+  //console.log("you are selected home");
+  //break;
+
+  //case 2:
+  //console.log("your are selceted about");
+  //break;
+
+  //case 3:
+  //console.log("your are selected condact");
+  //break;
+
+  //default:
+  //console.log("invaid option");
+
+//}
+
+
+
+//let dayNumber = 3;
+
+//switch(dayNumber){
+
+  //case 1:
+  //console.log("Monday");
+ // break;
+
+  //case 2:
+    //console.log("Tuesday");
+   // break;
+
+    //case 3:
+    //console.log("Wednesday");
+    //break;
+
+    //case 4:
+     // console.log("Thursday");
+     // break;
+
+      //case 5:
+       // console.log("Friday");
+        //break;
+
+        //case 6:
+          //console.log("Saturday");
+          //break;
+
+          //case 7:
+            //console.log("Sunday");
+            //break;
+
+            //default:
+              //console.log("Invalid day number.Please enter a number frm 1 To 7.");
+//}
+
+
+//let num1 = 10;
+//let num2 = 20;
+//let operator ="*";
+
+//let result;
+
+//switch(operator){
+  //case "*":
+    //result = num1 * num2;
+    //console.log(`result: ${num1} * ${num2} = ${result}`);
+    //break;
+//}
+
+
+//let Grade = "A";
+
+//switch(Grade){
+  //case "A":
+    //console.log("Exellent Great job");
+    //break;
+
+    //case "B":
+     // console.log("Very Good Keep it up");
+     // break;
+
+     // case "C":
+      //  console.log("Good! But there's room for improvment");
+       // break;
+
+        //case "D":
+         // console.log("You passed , But you need to work harder");
+       //  break;
+
+         // case "F":
+          //  console.log("Failed. Don't give up - try again");
+           // break;
+//}
+
+
+
+//for (let i = 1; i <=5; i++){
+  //console.log(i);
+//}
+
+
+//for (let e = 5; e <=165601; e++){
+  //console.log(e);
+//}
+
+
+//for (let w = 0; w <=10; w++){
+  //console.log("HEY ABIN");
+//}
+
+//for (let a = 5; a >=0; a--){
+  //console.log(a);
+//}
+
+//for (let i = 0; i <= 10; i += 2){
+  //console.log(i);
+//}
+
+//for (let a = 1; a <= 11; a +=5){
+  //console.log(a);
+//}
+
+
+//for (let a = 10; a >= 0; a -=2){
+  //console.log(a);
+//}
+
+
+//let fruits = ["apple","banana","cherry"];
+
+//for (let a = 0; a < fruits.length; a++){
+  //console.log(fruits[a]);
+//}
+
+
+//let cars = ["bmw", "benz", "jeep"];
+//for (let a = 1; a < cars.length; a++){
+  //console.log(cars[a]);
+//}
+
+
+//let names = ["appu","abin","sreee","jyhon","karthi","archh","vebve"];
+//for (let a = 5; a < names.length; a++){
+  //console.log(names[a]);
+//}
+
+//for (let i = 2; i <= 3; i++){
+  //for (let s = 2; s <= 3; i++){
+    //console.log(`${i} *${s} = ${i*s}`);
+  //}
+//}
+
+//for (let a = 1; a<=10; a++){
+  //if(a==8){
+  //  break;
+  //}
+ // console.log(a);
+//}
+
+
+//for (let a = 1; a <=10; a++){
+  //if(a == 5){
+   // continue;
+  //}
+  //console.log(a);
+//}
+
+//let sum = 0;
+//for (let a = 1; a <=10; a++){
+  //sum += a;
+//}
+//console.log("Sum:",sum);
+
+
+//et name = "Abin";
+//for(let i = 0; i < name.length; i++){
+  //console.log(name[i]);
+//}
+
+//let arr = [1,2,3,4,5];
+//or (let i = arr.length - 1; i >= 0; i--){
+  //console.log(arr[i]);
+//}
+
+//let i = 1;
+//while (i <=5){
+  //console.log(i);
+  //i++;
+//}
+
+//let count = 9;
+//while (count > 0){
+  //console.log("Countdown",count);
+  //count--;
+//}
+
+
+//let a = 1;
+//while (a <= 5) {
+  //console.log("Number:", a);
+  //a++;
+//}
+
+//let a = 9;
+//while (a >= 0){
+  //console.log("Number:", a);
+  //a--;
+//}
+
+//while (900){
+  //console.log("this time pass:900")
+//}
+
+
+//let a = 1;
+//while (a <=9){
+  //console.log(a);
+  //a++;
+//}
+
+//let a = 0;
+//while (true){
+  //a++;
+  //if (a === 101){
+    //break;
+  //}
+  //console.log("Count", a);
+//}
+
+
+//let a =2;
+//while (a <=10){
+  //console.log(a);
+  //a += 3;
+//}
+
+//let a = 1;
+//while (a <=4){
+  //let b =1;
+  //while (b <=4){
+    //console.log(`a = ${a}, b = ${b}`);
+    //b++;
+ //}
+  //a++;
+//}
+
+
+//let a = 1;
+//while(a <=10){
+  //console.log("abniji");
+  //a++;
+//}
+//console.log(a);
+
+//let a = 0;
+//do{
+  //console.log("hq",a);
+  //a++;
+  //}while(a<=5)
+
+  //for (let a=1;a<=5;a++)
+//{
+  //console.log("abin",a);
+  //for (let b=1;b<=5;b++)
+   // console.log("arcc",b);
+//}
+
+/// *ATM PROJECT* ///
+
+
+//let balance = 1000;
+ //function atmMenu(choice, amount) {
+  //switch (choice) {
+    //case 1:
+     //  console.log("Your balance is: ₹" + balance);
+     // break;
+    //case 2:
+      ////if (amount > 0) {
+      //  balance += amount;
+       // console.log("Deposited ₹" + amount + ". New balance: ₹" + balance);
+      //} else {
+       // console.log("Enter a valid amount to deposit.");
+      //}
+     // break;
+   // case 3:
+    //  if (amount > 0 && amount <= balance) {
+   //     balance -= amount;
+    //    console.log("Withdrawn ₹" + amount + ". New balance: ₹" + balance);
+    //  } else {
+    //    console.log("Invalid withdrawal amount or insufficient balance.");
+   //   }
+    //  break;
+    //case 4:
+    //  console.log("Exiting ATM. Thank you!");
+    //  break;
+   // default:
+   //   console.log("Invalid option selected.");
+ // }
+///}
+//for (let i = 0; i < 3; i++) {
+ // let option = i + 1;
+ // console.log("\nATM Transaction #" + option);
+  //atmMenu(1); // Check Balance
+  //atmMenu(2, 200); // Deposit
+ // atmMenu(3, 100); // Withdraw
+//}
+
+
+///* GRADE CALCULATOR*///
+
+
+//let studentName = "Abin";
+//let totalMark = 100;
+//const subjects = ["Math","Science","English"];
+//let mark = [98,90,88];
+//for (let i=0; i <subjects.length;i++) {
+  ///mark.push(mark);
+  //totalMark +=mark;
+//}
+//let average = totalMark / subjects.length;
+//let grade ="";
+
+//if (average >= 90){
+ // grade ="A";
+//}else if (average >=75){
+  //grade ="B";
+//}else if (average >=60){
+ // grade ="C";
+//}else{
+ // grade ="F";
+//}//
+
+//console.log("Report Card for:" + studentName);
+//onsole.log("Marks:"+mark);
+//console.log("Total:"+totalMark);
+//console.log("Average:"+average);
+//console.log("Grade:"+grade);
+
+
+// Grade Calculator
+
+//let studentName = "Abin";
+//let totalMarks = 0;
+//const subjects = ["Math", "Science", "English"];
+//let scores = [];
+
+//for (let i = 0; i < subjects.length; i++) {
+  //let mark = parseInt(prompt("Enter marks for " + subjects[i] + ":"));
+  //scores.push(mark);
+  //totalMarks += mark;
+//}
+
+// Average and Grade
+//let average = totalMarks / subjects.length;
+//let grade = "";
+
+//if (average >= 90) {
+  //grade = "A";
+//} else if (average >= 75) {
+ // grade = "B";
+////} else if (average >= 60) {
+ // grade = "C";
+//} else {
+ // grade = "F";
+//}
+
+//console.log("📋 Report Card for " + studentName);
+//console.log("Marks: " + scores.join(", "));
+//console.log("Total: " + totalMarks);
+//console.log("Average: " + average);
+//console.log("Grade: " + grade);
+
+
+//let correctUser = "abin";
+//let correntPass = "1234";
+
+//let inputUser = prompt("Ender username:");
+//let inputPass = prompt("Ender password:");
+
+//if (inputUser === correctUser && inputPass === correntPass){
+  //console.log("Login Successfull ! Welcome" + inputUser);
+
+//}else{
+  //console.log("Login Failed ! Username or Password is wrong");
+//}
+
+ 
+
+//let secretNumber = 7;
+//et attempts = 4;
+//while (attempts >0){
+  //let guess = parseInt(prompt("Guess a number between 1 and 10:"));
+  //if (guess === secretNumber){
+    //console.log("Correct ! You guessed the number");
+    //break;
+  //}else{
+   // attempts--;
+    //if (attempts > 0) {
+    //  console.log("Wron! Try agin"+ attempts);
+  //}else{
+   // console.log("Game Over" +secretNumber);
+  //}
+//}
+//}
+
+
+//function abin(){
+  //console.log("Hello");
+//}
+//abin();
+//abin();\\
+
+//function greet(name) {
+  //console.log("hello, "+ name);
+
+//}
+//greet("abin");
+
+//function greet(a,b){
+// console.log(a+b);
+//}
+//greet(10,20);
+//greet(20,30);
+
+//function abi(num){
+  //console.log("abi is " + num + " in " + (num*num));
+//}
+//abi(10);
+
+//function checkAge(age){
+  //if (age >= 18){
+    //console.log("You are eligible to vote.");
+  //}else{
+    //console.log("You are not eligible to vote.");
+  //}
+//}
+//checkAge(12);
+//checkAge(30);
+
+//function abi(a, b){
+// console.log(a * b);
+//}
+//abi(5000,50);
+
+//function abin(firstName, lastName){
+  //console.log("Full Name : " + firstName + " " + lastName);
+//}
+//abin("Abin", "vk");
+//abin("abinvk", "abin")
+ 
+//function Calculator(a, b, operators){
+  //if(operators ==="+"){
+   // console.log(a + b);
+  //}else if(operators ==="-"){
+  //  console.log(a - b);
+  //}else if(operators ==="*"){
+  //  console.log(a * b);
+ // }else if(operators ==="/"){
+  //  console.log(a / b);
+  //  console.log("Invalid opertors");
+ // }
+///
+
+//Calculator(10, 10, "*");
+//Calculator(10, 10, "/");
+//Calculator(10, 10, "-");
+//Calculator(10, 10, "+");
+
+//function discount(price, discount = 40){
+ // let finalPrice = price - (price * discount/200);
+  ///console.log("final price:" + finalPrice);
+//}
+//discount(1000);
+//discount(2000,2)
+
+//function charge(amount, charge = 20){
+  //console.log("total bill:"+(amount + charge));
+//}
+//charge(100);
+//charge(200,60)
+
